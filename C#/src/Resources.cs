3811f9db2e7c1ccb29ac34b3b846fbc9f8ed187c
Cole Severson <cseverso@uoregon.edu>
2019-11-04 00:32:24 -0800

    Added test project and one test

---
+++ C#/src/Resources.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures {
+    namespace Resources
+    {
+        public class Node<T>
+        {
+            public Node<T> Left{ get; set; }
+            public Node<T> Right{ get; set; }
+            public Node<T> Next{ get { return Left; } set { Left = value; } }
+            public T Data{ get; set; }
+            public Node(T data, Node<T> left=null, Node<T> right=null) {
+                this.Data = data;
+                this.Left = left;
+                this.Right = right;
+            }
+        }
+}
+}
